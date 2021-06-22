0885643944baf58523d119fefba1eb98fe638edd
Rebeca Ito <emi.ito@edu.unifil.br>
2021-06-22 14:03:04 -0300

    commit

---
+++ example-app/resources/lang/pt-BR/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'failed' => 'Essas credenciais são inválidas.',
+    'password' => 'A senha inserida está incorreta.',
+    'throttle' => 'Muitas tentativas de acesso. Tentar novamente em :segunds segunds.',
+
+];
---
+++ example-app/resources/lang/pt-BR/trackPets.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    'menu' => [
+        'main' => 'Menu Principal',
+    ]
+];
